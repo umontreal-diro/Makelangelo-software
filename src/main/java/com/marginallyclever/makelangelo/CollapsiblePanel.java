3dcfe221c090a6113ef263c76a73b2a8aaa71299
Laura Cadillo <lauracadillo13@gmail.com>
2024-10-02 19:10:21 -0400

    Revert "collapsible panel test"
    This reverts commit 054887a115da2fc26ab5fc46727ee33af49d7b6e.

--- src/main/java/com/marginallyclever/makelangelo/CollapsiblePanel.java
+++ src/main/java/com/marginallyclever/makelangelo/CollapsiblePanel.java
@@ -13,7 +13,7 @@ import java.awt.event.*;
 public class CollapsiblePanel extends JPanel {
 	private String title;
     private final TitledBorder border;
-    final JPanel innerPanel;
+    private final JPanel innerPanel;
     private final Window parentWindow;
     private Dimension previousDimension;
     private final int heightCollapsibleComponent;
@@ -215,12 +215,9 @@ public class CollapsiblePanel extends JPanel {
         cpanel.add(i);
         cpanel.add(j);
 
-
-
         frame.setPreferredSize(new Dimension(600, 90));
         frame.add(jPanel);
         frame.pack();
         frame.setVisible(true);
-
     }
 }
