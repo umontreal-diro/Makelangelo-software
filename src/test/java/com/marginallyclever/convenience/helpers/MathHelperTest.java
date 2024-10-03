049db2c22ba715ef4d54b604ce553d4f4a140c64
lalogiquefloue <lalogiquefloue@gmail.com>
2024-10-03 19:46:04 -0400

    test MathHelper equals method

--- src/test/java/com/marginallyclever/convenience/helpers/MathHelperTest.java
+++ src/test/java/com/marginallyclever/convenience/helpers/MathHelperTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.vecmath.Point2d;
+import javax.vecmath.Tuple2d;
+import javax.vecmath.Vector2d;
 
 public class MathHelperTest {
     @Test
@@ -83,5 +85,29 @@ public class MathHelperTest {
             c.y+=ortho.x;
             Assertions.assertFalse(MathHelper.between(a, b, c, epsilon));
         }
+    }
+
+    // NEW TEST
+    @Test
+    void testEquals() {
+        Tuple2d a0 = new Vector2d(0.0,0.0);
+        Tuple2d a1 = new Vector2d(1.0,0.0);
+        Tuple2d b0 = new Vector2d(0.0,0.0);
+        Tuple2d b1 = new Vector2d(1.0,0.0);
+        Assertions.assertTrue(MathHelper.equals(a0, a1, b0, b1, 0.01));
+        Assertions.assertFalse(MathHelper.equals(a0, a1, b0, b1, 0.0));
+
+        Tuple2d c0 = new Vector2d(0.0,0.09);
+        Tuple2d c1 = new Vector2d(1.0,0.09);
+        Assertions.assertFalse(MathHelper.equals(c0, c1, a0, a1, 0.08));
+        Assertions.assertTrue(MathHelper.equals(c0, c1, a0, a1, 0.1));
+        Assertions.assertFalse(MathHelper.equals(c0, c1, a0, a1, 0.09));
+
+        Tuple2d d0 = new Vector2d(0.0,0.0);
+        Tuple2d d1 = new Vector2d(0.0,0.0);
+        Tuple2d e0 = new Vector2d(0.0,0.0);
+        Tuple2d e1 = new Vector2d(0.0,0.0);
+        Assertions.assertTrue(MathHelper.equals(d0, d1, e0, e1 ,0.1));
+        Assertions.assertFalse(MathHelper.equals(d0, d1, e0, e1 ,0.0));
     }
 }
