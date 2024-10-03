4f5cab99f577281331712224654c125f9692a726
Laura Cadillo <lauracadillo13@gmail.com>
2024-10-03 16:41:28 -0400

    Revert "Update SelectPasswordTest.java"
    This reverts commit 53bd1f6d424ec84bc43778f862766f0e1aa232e2.

--- src/test/java/com/marginallyclever/makelangelo/select/SelectPasswordTest.java
+++ src/test/java/com/marginallyclever/makelangelo/select/SelectPasswordTest.java
@@ -1,53 +1,20 @@
 package com.marginallyclever.makelangelo.select;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertSame;
 import java.awt.Color;
 import java.awt.color.ColorSpace;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class SelectPasswordTest {
-  /**
-   * Method under test: {@link SelectPassword#getPassword()}
-   */
-  @Test
-  void testGetPassword() {
-    // Arrange, Act and Assert
-    assert "Default Text".equals((new SelectPassword("Internal Name", "Label Key", "Default Text")).getPassword());
-  }
-
-  /**
-   * Method under test: {@link SelectPassword#isEditable()}
-   */
-  @Test
-  void testIsEditable() {
-    // Arrange, Act and Assert
-    assert (new SelectPassword("Internal Name", "Label Key", "Default Text")).isEditable();
-  }
-
-  /**
-   * Method under test: {@link SelectPassword#getDragEnabled()}
-   */
-  @Test
-  void testGetDragEnabled() {
-    // Arrange, Act and Assert
-    assert !(new SelectPassword("Internal Name", "Label Key", "Default Text")).getDragEnabled();
-  }
-
-
   /**
    * Method under test:
    * {@link SelectPassword#SelectPassword(String, String, String)}
    */
   @Test
   void testNewSelectPassword() {
-    // Arrange, Act
+    // Arrange, Act and Assert
     Color background = (new SelectPassword("Internal Name", "Label Key", "Default Text")).getBackground();
     ColorSpace expectedColorSpace = background.getColorSpace();
-
-    // Assert
-    assert expectedColorSpace == background.darker().getColorSpace();
+    assertSame(expectedColorSpace, background.darker().getColorSpace());
   }
 }
