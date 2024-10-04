7f38687cf93b5ff521736b08815c79b8341d07e5
lalogiquefloue <lalogiquefloue@gmail.com>
2024-10-03 20:30:35 -0400

    Plotter class tests, to be completed

---
+++ src/test/java/com/marginallyclever/makelangelo/plotter/PlotterTest.java
@@ -0,0 +1,35 @@
+package com.marginallyclever.makelangelo.plotter;
+
+import com.marginallyclever.convenience.Point2D;
+import com.marginallyclever.makelangelo.turtle.MovementType;
+import com.marginallyclever.makelangelo.turtle.TurtleMove;
+import com.marginallyclever.util.PreferencesHelper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.vecmath.Vector2d;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlotterTest {
+
+    Plotter plotter;
+
+    @BeforeEach
+    void setUp() {
+        PreferencesHelper.start();
+        plotter = new Plotter();
+    }
+
+    @Test
+    void turtleMove() {
+        TurtleMove m0 = new TurtleMove(0.0, 10.0, MovementType.TRAVEL);
+        TurtleMove m1 = new TurtleMove(0.0, 0.0, MovementType.TRAVEL);
+
+        plotter.turtleMove(m0);
+        plotter.turtleMove(m1);
+        Point2D expected = new Point2D(0.0, 0.0);
+        Assertions.assertEquals(expected.x, plotter.getPos().x);
+    }
+}
