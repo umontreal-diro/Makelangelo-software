a9b649f6d3d96a5e644cfa4e70a681995ecba367
lalogiquefloue <lalogiquefloue@gmail.com>
2024-10-06 14:57:41 -0400

    added test cases to PlotterTest

--- src/test/java/com/marginallyclever/makelangelo/plotter/PlotterTest.java
+++ src/test/java/com/marginallyclever/makelangelo/plotter/PlotterTest.java
@@ -8,10 +8,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.vecmath.Vector2d;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class PlotterTest {
 
     Plotter plotter;
@@ -23,14 +19,72 @@ class PlotterTest {
     }
 
     @Test
-    void turtleMove() {
-        TurtleMove m0 = new TurtleMove(0.0, 10.0, MovementType.TRAVEL);
+    void turtleTravel() {
+        TurtleMove m0 = new TurtleMove(10.0, 10.0, MovementType.TRAVEL);
         TurtleMove m1 = new TurtleMove(0.0, 0.0, MovementType.TRAVEL);
 
         plotter.turtleMove(m0);
         plotter.turtleMove(m1);
-        Point2D expected = new Point2D(0.0, 0.0);
-        Assertions.assertEquals(expected.x, plotter.getPos().x);
-        Assertions.assertEquals(expected.y, plotter.getPos().y);
+
+        Point2D expectedPosition = new Point2D(0.0, 0.0);
+        Assertions.assertEquals(expectedPosition.x, plotter.getPos().x);
+        Assertions.assertEquals(expectedPosition.y, plotter.getPos().y);
+        Assertions.assertTrue(plotter.getPenIsUp());
+    }
+
+    @Test
+    void turtleMove() {
+        TurtleMove m0 = new TurtleMove(10.0, 10.0, MovementType.DRAW_LINE);
+        TurtleMove m1 = new TurtleMove(0.0, 0.0, MovementType.DRAW_LINE);
+
+        plotter.turtleMove(m0);
+        plotter.turtleMove(m1);
+
+        Point2D expectedPosition = new Point2D(0.0, 0.0);
+        Assertions.assertEquals(expectedPosition.x, plotter.getPos().x);
+        Assertions.assertEquals(expectedPosition.y, plotter.getPos().y);
+        Assertions.assertFalse(plotter.getPenIsUp());
+    }
+
+    @Test
+    void turtleFindHome(){
+        TurtleMove m0 = new TurtleMove(10.0, 10.0, MovementType.DRAW_LINE);
+
+        plotter.turtleMove(m0);
+        plotter.findHome();
+
+        Point2D expectedPosition = new Point2D(0.0, 0.0);
+        Assertions.assertEquals(expectedPosition.x, plotter.getPos().x);
+        Assertions.assertEquals(expectedPosition.y, plotter.getPos().y);
+        Assertions.assertTrue(plotter.getPenIsUp());
+        Assertions.assertTrue(plotter.getDidFindHome());
+    }
+
+    @Test
+    void turtleComplexCommandSequence(){
+        TurtleMove m0 = new TurtleMove(10.0, 10.0, MovementType.TRAVEL);
+        TurtleMove m1 = new TurtleMove(-10.0, -10.0, MovementType.DRAW_LINE);
+        TurtleMove m2 = new TurtleMove(12.3, 32.1, MovementType.TRAVEL);
+
+        plotter.turtleMove(m0);
+        plotter.findHome();
+        plotter.turtleMove(m1);
+        plotter.turtleMove(m2);
+
+        Point2D expectedPosition = new Point2D(12.3, 32.1);
+        Assertions.assertEquals(expectedPosition.x, plotter.getPos().x);
+        Assertions.assertEquals(expectedPosition.y, plotter.getPos().y);
+        Assertions.assertTrue(plotter.getPenIsUp());
+    }
+
+    @Test
+    void turtleToolChange(){
+        TurtleMove m0 = new TurtleMove(10.0, 10.0, MovementType.TOOL_CHANGE);
+
+        plotter.turtleMove(m0);
+
+        Point2D expectedPosition = new Point2D(0.0, 0.0);
+        Assertions.assertEquals(expectedPosition.x, plotter.getPos().x);
+        Assertions.assertEquals(expectedPosition.y, plotter.getPos().y);
     }
 }
