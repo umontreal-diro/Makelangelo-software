d501a18e9fb7224b4624941a2b13323a31dd85d5
OO <ouqiyun2021@gmail.com>
2024-10-08 11:27:25 -0400

    commentaires

--- src/test/java/com/marginallyclever/makelangelo/plotter/plottercontrols/ActionCommandDialogTest.java
+++ src/test/java/com/marginallyclever/makelangelo/plotter/plottercontrols/ActionCommandDialogTest.java
@@ -14,6 +14,8 @@ class ActionCommandDialogTest {
         dialog = new ActionCommandDialog();
     }
 
+    /* Teste la méthode saveResult lorsque la sélection est nulle.
+     * Il signifie que la boîte de dialogue est fermée lorsque rien n'est sélectionné.*/
     @Test
     public void testSaveResult_NullSelection() {
         JOptionPane pane = new JOptionPane();
@@ -21,6 +23,9 @@ class ActionCommandDialogTest {
         assertEquals(JOptionPane.CLOSED_OPTION, result, "Result should be CLOSED_OPTION when selection is null.");
     }
 
+    /* Teste la méthode saveResult lorsque des choix sont fournis. La méthode saveResult est appelée après avoir
+     * simulé la sélection de "Choice 2" par l'utilisateur. Le test vérifie que le résultat renvoyé est 1,
+     * ce qui correspond à l'indice de "Choice 2" dans le tableau des choix.*/
     @Test
     public void testSaveResult_WithChoices() {
         JOptionPane pane = new JOptionPane();
--- src/test/java/com/marginallyclever/makelangelo/plotter/plottercontrols/TextInterfaceToListenersTest.java
+++ src/test/java/com/marginallyclever/makelangelo/plotter/plottercontrols/TextInterfaceToListenersTest.java
@@ -14,6 +14,7 @@ public class TextInterfaceToListenersTest {
         textInterface = new TextInterfaceToListeners();
     }
 
+    /* Teste la méthode sendCommand pour s'assurer qu'elle envoie la commande correcte via un ActionListener.*/
     @Test
     public void testSendCommand() {
         ActionListener listener = new ActionListener() {
@@ -27,6 +28,7 @@ public class TextInterfaceToListenersTest {
         textInterface.removeActionListener(listener);
     }
 
+    /* Teste la méthode getCommand pour s'assurer qu'elle retourne la commande correcte.*/
     @Test
     public void testGetCommand() {
         textInterface.setCommand("test command");
