7ed9655019c4808df5c334d5e284db773a6a0656
Ilovenut <96840041+Ilovenut@users.noreply.github.com>
2024-10-08 14:12:30 -0400

    Update TestColorRGB.java

--- src/test/java/com/marginallyclever/convenience/TestColorRGB.java
+++ src/test/java/com/marginallyclever/convenience/TestColorRGB.java
@@ -2,6 +2,7 @@ package com.marginallyclever.convenience;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import com.github.javafaker.Faker;
 
 public class TestColorRGB {
     private void assertColorsEqual(ColorRGB c,int r,int g,int b) {
@@ -14,4 +15,26 @@ public class TestColorRGB {
     public void test() {
         assertColorsEqual(new ColorRGB(0xff123456),0x12,0x34,0x56);
     }
+
+    @Test
+    public void testWithFaker() {
+        //Nous testons la classe  ColorRgb car elle gère les représentations de couleurs RGB, essentielles pour
+        // le rendu graphique de l'application. Assurer la précision des opérations sur les couleurs, telles que
+        // l'addition, la soustraction et la conversion en entier ou en chaîne hexadécimale, est crucial pour
+        // maintenir la cohérence visuelle et éviter les erreurs de couleur qui pourraient nuire à
+        // l'expérience utilisateur.
+        // Arrange
+        Faker faker = new Faker();
+        int expectedRed = faker.number().numberBetween(0, 256);
+        int expectedGreen = faker.number().numberBetween(0, 256);
+        int expectedBlue = faker.number().numberBetween(0, 256);
+
+        // Act
+        ColorRGB color = new ColorRGB(expectedRed, expectedGreen, expectedBlue);
+
+        // Assert
+        assertColorsEqual(color, expectedRed, expectedGreen, expectedBlue);
+    }
+
+
 }
